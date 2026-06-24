68c907da0a2c25d33d19222246ffa20b287c6108
MiguelWanderer <subterra.1999@gmail.com>
2026-06-24 11:37:24 -0400

    Modificacion del Css

--- src/components/01_inicio_AstJos.jsx
+++ src/components/01_inicio_AstJos.jsx
@@ -3,9 +3,7 @@ export default function InicioAstJos() {
     <article className="doc-article">
       <h1>Inicio de la Wiki AstJos</h1>
       <p>
-        Esta wiki reúne la base de trabajo para la implantación y administración de
-        los servicios de AstJos. Cada sección está pensada para crecer con notas,
-        procedimientos y referencias rápidas.
+        Esta wiki reúne Los conocimientos para señirnos a la Undiad 2 de el Curso de Sistemas Operativos. Cada sección esta pensanda para crear un Paso a paso de todos nuestros procesos para lograr la correcta Instalacion y funcionamiento de Windows Server.
       </p>
 
       <section className="doc-section">
