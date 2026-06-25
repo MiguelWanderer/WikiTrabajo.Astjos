d2a072caa3b0c37833d4bd146415c1f6c72c1ee9
MiguelWanderer <subterra.1999@gmail.com>
2026-06-25 03:54:11 -0400

    WIP Instalacion

--- src/components/02_instalacion_AstJos.jsx
+++ src/components/02_instalacion_AstJos.jsx
@@ -13,17 +13,17 @@ export default function InstalacionAstJos() {
           <li>1.-Definir los elementos que nesecitamos descargar</li>
           <li>2.-Descargar los elementos necesarios.</li>
           <li>3.-Verificamos que Nuestra Iso de Windows,Windows server 2025 esten correctamente descargadas</li>
-          <li>5.-Configurar el sistema operativo.Con un usuario especifico y con una contraseña segura y que repetiremos en el resto del proceso</li>
-          <li>6.-Configurar el sistema operativo.Con un Minnomo de 50 gb de espacio en disco, 4 gb minimos de ram, 2 cores de procesador</li>
-          <li>7.-Configurar el sistema operativo.Con una red propia para este Laboratorio, que sera llamadad REDLAB</li>
-          <li>4.-Instalamos el sistema Operativo de Windows server 2025 en la maquina virtual(Virtual vox)</li>
-          <li>Importante la ISO debe ser la experiencia de escritorio en español para los propósitos del laboratorio.</li>
-          <li>8.-Esperar a que se complete la instalación.</li>
-          <li>9.-Configura el nombre del servidor local que sera "SRV-DC01" para poder identificarlo fácilmente.</li>
-          <li>10.-Reiniciamos</li>
-          <li>11.-Configuramos el sistema de la Ip fija en el adaptador: direccion 192.168.10.10 con mascara de 255.255.255.0 con puerta de enlace vacia y Dns preferido de : 127.0.0.1</li>
-          <li>12.-Finalmente comprobamos la configuracion de el Firewal que debe encontrarse activo</li>
-          <li>13.-Fin de los Pasos desde cero hacia el fin del Punto A</li>
+          <li>4.-Configurar el sistema operativo.Con un usuario especifico y con una contraseña segura y que repetiremos en el resto del proceso</li>
+          <li>5.-Configurar el sistema operativo.Con un Minnomo de 50 gb de espacio en disco, 4 gb minimos de ram, 2 cores de procesador</li>
+          <li>6.-Configurar el sistema operativo.Con una red propia para este Laboratorio, que sera llamadad REDLAB</li>
+          <li>7.-Instalamos el sistema Operativo de Windows server 2025 en la maquina virtual(Virtual vox)</li>
+          <li>8.-Importante la ISO debe ser la experiencia de escritorio en español para los propósitos del laboratorio.</li>
+          <li>9.-Esperar a que se complete la instalación.</li>
+          <li>10.-Configura el nombre del servidor local que sera "SRV-DC01" para poder identificarlo fácilmente.</li>
+          <li>11.-Reiniciamos</li>
+          <li>12.-Configuramos el sistema de la Ip fija en el adaptador: direccion 192.168.10.10 con mascara de 255.255.255.0 con puerta de enlace vacia y Dns preferido de : 127.0.0.1</li>
+          <li>13.-Finalmente comprobamos la configuracion de el Firewal que debe encontrarse activo</li>
+          <li>14.-Fin de los Pasos desde cero hacia el fin del Punto A</li>
         </ol>
 
         <div style={{ marginTop: '30px' }}>
