dbfb6f25a316834a1262d0477b8a513ded3ccf96
MiguelWanderer <subterra.1999@gmail.com>
2026-06-25 03:15:50 -0400

    WIP Instalacion

--- src/components/02_instalacion_AstJos.jsx
+++ src/components/02_instalacion_AstJos.jsx
@@ -8,15 +8,21 @@ export default function InstalacionAstJos() {
       </p>
 
       <section className="doc-section">
-        <h2>Pasos base</h2>
+        <h2>Pasos desdes 0 a Final de Punto A</h2>
         <ol>
           <li>1.-Definir los elementos que nesecitamos descargar</li>
           <li>2.-Descargar los elementos necesarios.</li>
           <li>3.-Verificamos que Nuestra Iso de Windows,Windows server 2025 esten correctamente descargadas</li>
-          <li>4.-Instalamos el sistema Operativo de Windows server 2025</li>
-          <li>5.-Asignar nombres de host y direcciones IP estáticas.</li>
-          <li>6.-Configurar DNS y un rango de direcciones para DHCP.</li>
-          <li>7.-Instalar los roles de servidor necesarios.</li>
+          <li>5.-Configurar el sistema operativo.Con un usuario especifico y con una contraseña segura y que repetiremos en el resto del proceso</li>
+          <li>6.-Configurar el sistema operativo.Con un Minnomo de 50 gb de espacio en disco, 4 gb minimos de ram, 2 cores de procesador</li>
+          <li>7.-Configurar el sistema operativo.Con una red propia para este Laboratorio, que sera llamadad REDLAB</li>
+          <li>4.-Instalamos el sistema Operativo de Windows server 2025 en la maquina virtual(Virtual vox)</li>
+          <li>Importante la ISO debe ser la experiencia de escritorio en español para los propósitos del laboratorio.</li>
+          <li>8.-Esperar a que se complete la instalación.</li>
+          <li>9.-Configura el nombre del servidor local que sera "SRV-DC01" para poder identificarlo fácilmente.</li>
+          <li>10.-Reiniciamos</li>
+          <li>11.-Configuramos el sistema de la Ip fija en el adaptador: direccion 192.168.10.10 con mascara de 255.255.255.0 con puerta de enlace vacia y Dns preferido de : 127.0.0.1</li>
+          <li>12.-Finalmente comprobamos la configuracion de el Firewal que debe encontrarse activo</li>
         </ol>
       </section>
 
