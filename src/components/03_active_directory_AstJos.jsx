c47f35164b5c1343ac90136fb1155765f2ae1b94
MiguelWanderer <subterra.1999@gmail.com>
2026-06-25 04:16:14 -0400

    WIP active directory

--- src/components/03_active_directory_AstJos.jsx
+++ src/components/03_active_directory_AstJos.jsx
@@ -8,12 +8,16 @@ export default function ActiveDirectoryAstJos() {
       </p>
 
       <section className="doc-section">
-        <h2>Elementos clave</h2>
+        <h2>Elementos clave de PASOS B</h2>
         <ul>
-          <li>Dominio principal y bosque.</li>
-          <li>Unidades organizativas para separar equipos y usuarios.</li>
-          <li>Cuentas de servicio y cuentas de usuario.</li>
-          <li>Grupos para simplificar permisos.</li>
+          <li>Dirigirnos a administrador del servidor luego a administrar para presionar en agregar roles y caracteristicas</li>
+          <li>Marcar AD DS e instalar</li>
+          <li>Promocionar el servidor a controlador de dominio esto en la bandera de notificacion.</li>
+          <li>Agregar un nuevo bosque</li>
+          <li>Dominio raiz inacap.local</li>
+          <li>El DNS se instala automaticamente</li>
+          <li>Reiniciar</li>
+          <li>Iniciamos sesion  con INACAP\Administrador para los propósitos del laboratorio.</li>
         </ul>
       </section>
     </article>
